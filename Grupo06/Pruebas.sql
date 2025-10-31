52498d632a618d16d8c6eeb4885c8e959119d384
GuadaGambaro <ggambarolartigue@hotmail.com>
2025-10-31 12:25:50 -0300

    Agregue el importe de datos a la tabla Administracion en Pruebas.sql

--- Grupo06/Pruebas.sql
+++ Grupo06/Pruebas.sql
@@ -2,5 +2,65 @@
 -- Lu (~-~)
 -- jaure UWU :3 
 -- Javi
+
+
+
 --Guada
+
+
+----------------------------------------SP IMPORTACION DE DATOS TABLA ADMINISTRACION------------------------------
+
+
+--ESTO IRIA EN LA CONSULTA DE "GenerarObjetos.sql"
+
+IF NOT EXISTS (
+    SELECT * FROM sys.objects 
+    WHERE object_id = OBJECT_ID(N'tp.ImportarAdministracion') AND type = 'P')
+BEGIN
+    EXEC('CREATE PROCEDURE tp.ImportarAdministracion AS BEGIN SET NOCOUNT ON; END') --SE NECESITA SQL DINAMICO PORQUE SQL NO PERMITE CREAR UN SP DENTRO DE UN BLOQUE CONDICIONAL DIRECTAMENTE
+END
+GO
+
+ALTER PROCEDURE tp.ImportarAdministracion
+AS
+BEGIN
+
+    SET NOCOUNT ON; --NO MUESTRA LAS FILAS AFECTADAS. MEJORA EL RENDIMIENTO
+
+    DECLARE @Cantidad INT = FLOOR(RAND() * 8) + 3; --NUMERO ALEATORIO ENTRE 3 Y 10
+
+
+    INSERT INTO tp.Administracion (Nombre, Direccion, CorreoElectronico, Telefono)
+    SELECT TOP (@Cantidad) * --SELECCIONA UN CANTIDAD ALEATORIA DE FILAS
+    FROM (
+        VALUES
+            ('Admin Uno', 'Calle 123', 'admin1@email.com', '1111111111'),
+            ('Admin Dos', 'Av. Siempre Viva', 'admin2@email.com', '2222222222'),
+            ('Admin Tres', 'Calle Falsa 456', 'admin3@email.com', '3333333333'),
+            ('Admin Cuatro', 'Diagonal 74', 'admin4@email.com', '4444444444'),
+            ('Admin Cinco', 'Ruta 8 KM 45', 'admin5@email.com', '5555555555'),
+            ('Admin Seis', 'Calle 9', 'admin6@email.com', '6666666666'),
+            ('Admin Siete', 'Av. Libertador', 'admin7@email.com', '7777777777'),
+            ('Admin Ocho', 'Calle 10', 'admin8@email.com', '8888888888'),
+            ('Admin Nueve', 'Calle 11', 'admin9@email.com', '9999999999'),
+            ('Admin Diez', 'Calle 12', 'admin10@email.com', '1010101010')
+
+    ) AS Datos (Nombre, Direccion, CorreoElectronico, Telefono)
+
+    WHERE NOT EXISTS (
+        SELECT 1 FROM tp.Administracion a WHERE a.Nombre = Datos.Nombre --EVITA DATOS DUPLICADOS
+    )
+    ORDER BY NEWID(); --PERMITE ALEATORIEDAD
+END;
+GO
+
+
+--ESTO IRIA EN LA CONSULTA DE "InsertarDatos.sql"
+
+EXEC tp.ImportarAdministracion
+
+
+
+
+
 --Majo
